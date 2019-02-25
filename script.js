62c771f5bc8df4a79db6f9f9e008388c507d1cf6
Adela Selimović <47861513+adelaselimovic@users.noreply.github.com>
2019-02-25 04:51:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+  var br=0;
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function myFunction1(){
+  var x = document.getElementById("top_shows");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
